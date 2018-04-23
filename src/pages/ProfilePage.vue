83c77c045814f0d849416d7be1226d841548983a
Waqar Shahid <waqarshahid.nuces@yahoo.com>
2018-04-23 19:11:17 +0500

    profile id added, employee redirects to profile path using id

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="profile-title">
+      Links associated to {{ profile.name }}, ({{ profile.designation }})
+    </div>
+    <div class="links">
+      {{ profile.designation }}
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    mounted () {
+      this.getAllEmployees()
+    },
+    computed: {
+      ...mapGetters([
+        'allEmployees'
+      ]),
+      profile () {
+        let id = parseInt(this.$route.params.id)
+        return this.allEmployees.find((p) => p.id === id) || {}
+      }
+    },
+    methods: {
+      ...mapActions([
+        'getAllEmployees'
+      ])
+    }
+  }
+</script>
+
+<style>
+
+.profile-title {
+  padding-top: 20px;
+  margin: 0 auto;
+  font-size: 26px;
+}
+
+.links {
+  float: left;
+  font-size: 20px;
+  margin-top: 15px;
+}
+
+</style>
